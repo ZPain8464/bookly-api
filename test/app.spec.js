39ae9b36767d2f9840966ee0a8b457edce7eba93
Zach Painter <z.painter45@gmail.com>
2020-12-12 09:43:02 -0600

    initial commit

---
+++ test/app.spec.js
@@ -0,0 +1,7 @@
+const app = require("../src/app");
+
+describe("App", () => {
+  it('GET / responds with 200 containing "Hello, world!"', () => {
+    return supertest(app).get("/api/bookly").expect(200, "Hello, bookly!");
+  });
+});
