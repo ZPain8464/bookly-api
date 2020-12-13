fef6b603a05dc0fbc422417b29f31a9799ac0fce
Zach Painter <z.painter45@gmail.com>
2020-12-13 13:19:16 -0600

    added teams endpoints

--- src/Teams/teams-router.js
+++ src/Teams/teams-router.js
@@ -35,4 +35,50 @@ teamsRouter
       .catch(next);
   });
 
+teamsRouter
+  .route("/:id")
+  .all((req, res, next) => {
+    TeamsService.getById(req.app.get("db"), req.params.id)
+      .then((team) => {
+        if (!team) {
+          return res.status(404).json({
+            error: { message: `Team doesn't exist` },
+          });
+        }
+        res.team = team;
+        next();
+      })
+      .catch(next);
+  })
+  .get((req, res, next) => {
+    res.json(res.team);
+  })
+  .delete((req, res, next) => {
+    TeamsService.deleteTeam(req.app.get("db"), req.params.id)
+      .then((numRowsAffected) => {
+        res.status(204).end();
+      })
+      .catch(next);
+  })
+  .patch((req, res, next) => {
+    const { title } = req.body;
+    const teamToUpdate = { title };
+
+    const numberOfValues = Object.values(teamToUpdate).filter(Boolean).length;
+    if (numberOfValues === 0) {
+      logger.error(`Invalid update without required fields`);
+      return res.status(400).json({
+        error: {
+          message: `Request body must contain either 'title'`,
+        },
+      });
+    }
+
+    TeamsService.updateTeam(req.app.get("db"), req.params.id, teamToUpdate)
+      .then((numRowsAffected) => {
+        res.status(204).end();
+      })
+      .catch(next);
+  });
+
 module.exports = teamsRouter;
--- src/Teams/teams-service.js
+++ src/Teams/teams-service.js
@@ -3,6 +3,10 @@ const TeamsService = {
     return knex.from("teams").select("*");
   },
 
+  getById(knex, id) {
+    return knex.from("teams").select("*").where("id", id).first();
+  },
+
   insertTeam(knex, newTeam) {
     return knex
       .insert(newTeam)
@@ -11,6 +15,14 @@ const TeamsService = {
       .then((rows) => {
         return rows[0];
       });
+  },
+
+  deleteTeam(knex, id) {
+    return knex("teams").where({ id }).delete();
+  },
+
+  updateTeam(knex, id, newTeamFields) {
+    return knex("teams").where({ id }).update(newTeamFields);
   },
 };
 
