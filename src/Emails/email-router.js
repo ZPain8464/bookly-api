b9f98739427a979af497730f308969e82de22c42
Zach Painter <z.painter45@gmail.com>
2021-01-04 21:01:09 -0600

    updated email-router to send confirmation message to FE

--- src/Emails/email-router.js
+++ src/Emails/email-router.js
@@ -6,29 +6,27 @@ sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 const { requireAuth } = require("../middleware/jwt-auth");
 
 const emailsRouter = express.Router();
-
 emailsRouter.route("/").post(requireAuth, (req, res, next) => {
-  console.log(req.query);
   // Get variables from query string
-  const { recipient, sender, senderName } = req.body;
-  const url = "http://localhost:3000";
+  const { recipient, sender, name } = req.body;
+  const url = "http://localhost:3000/invite-page";
 
-  const subject = senderName;
-  //   + ` invited you to join Bookly!`;
-  const text = `${senderName} wants to add you to their team on Bookly so you can view and join events. If you don't have an account, go ahead and click the link below to get signed up:`;
-  console.log(recipient);
+  const subject = `${name} invited you to join Bookly!`;
+  const text = `${name} wants to add you to their team on Bookly so you can view and join events. If you don't have an account, go ahead and click the link below to get signed up:${url}`;
   //SendGrid data requirements
   const msg = {
     to: recipient,
     from: sender,
-    subject: "sender",
-    text: `Zachary wants to add you to their team on Bookly so you can view and join events. If you don't have an account, go ahead and click the link to get signed up: ${url}`,
+    subject: subject,
+    text: `${name} wants to add you to their team on Bookly so you can view and join events. If you don't have an account, go ahead and click the link to get signed up: ${url}`,
   };
 
   //Send email
   sgMail
     .send(msg)
-    .then((msg) => console.log(text))
+    .then((msg) => {
+      return res.status(201).json("email successfully sent");
+    })
     .catch();
 });
 
