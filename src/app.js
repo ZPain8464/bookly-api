0b17beab249e4eee4d39dd09071fbe0819218031
Zach Painter <z.painter45@gmail.com>
2021-01-24 16:14:04 -0600

    added cors function to app.js

--- src/app.js
+++ src/app.js
@@ -25,6 +25,15 @@ app.use(
   })
 );
 
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 app.use("/api/auth", authRouter);
 app.use("/api/users", usersRouter);
 app.use("/api/events", eventsRouter);
