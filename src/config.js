4fcf37dfe0779d9a6bda718bd8e2576568e4af5a
Zach Painter <z.painter45@gmail.com>
2021-01-23 18:18:32 -0600

    added CLIENT_ORIGIN to config

--- src/config.js
+++ src/config.js
@@ -2,6 +2,7 @@ module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || "development",
   API_TOKEN: process.env.API_TOKEN,
+  CLIENT_ORIGIN=process.env.CLIENT_ORIGIN,
   JWT_SECRET: "bookly-token",
   DATABASE_URL: process.env.DATABASE_URL || process.env.TEST_DATABASE_URL,
   SENDGRID_API_KEY: process.env.SENDGRID_API_KEY,
