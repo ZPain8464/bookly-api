0a9081d147fe9c4716a900c22088b76ae7ae0b94
Zach Painter <z.painter45@gmail.com>
2021-01-24 15:13:20 -0600

    added CLIENT_ORIGIN to app.js

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
+const { CLIENT_ORIGIN } = require("./config");
 const helmet = require("helmet");
 const { NODE_ENV } = require("./config");
 const eventsRouter = require("./Events/events-router");
@@ -18,7 +19,11 @@ const morganOption = NODE_ENV === "production" ? "tiny" : "common";
 app.use(morgan(morganOption));
 app.use(helmet());
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: CLIENT_ORIGIN,
+  })
+);
 
 app.use("/api/auth", authRouter);
 app.use("/api/users", usersRouter);
