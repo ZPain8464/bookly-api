ed74c03bfa9b5445aafca8f612a870ff7e67e20d
Zach Painter <z.painter45@gmail.com>
2021-02-01 14:49:41 -0600

    removed client_origin from config

--- src/config.js
+++ src/config.js
@@ -2,7 +2,6 @@ module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || "development",
   API_TOKEN: process.env.API_TOKEN,
-  CLIENT_ORIGIN: process.env.CLIENT_ORIGIN,
   JWT_SECRET: "bookly-token",
   DATABASE_URL:
     process.env.DATABASE_URL || "postgresql://apple@localhost/bookly_db",
