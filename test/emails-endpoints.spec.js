ea90fd195fd796afd03e27da32e7af4adae1fb44
Zach Painter <z.painter45@gmail.com>
2021-02-01 10:25:48 -0600

    updated DB url due to Heroku maintenance

--- test/emails-endpoints.spec.js
+++ test/emails-endpoints.spec.js
@@ -2,6 +2,7 @@ const { expect } = require("chai");
 const knex = require("knex");
 const supertest = require("supertest");
 const app = require("../src/app");
+const { inviteArray } = require("./emails.fixtures");
 
 const EmailsService = require("../src/Emails/emails-service");
 
@@ -15,4 +16,20 @@ describe(`Emails service object`, function () {
     });
     app.set("db", db);
   });
+
+  //   beforeEach("register and login", () => {
+  //     let invites = inviteArray();
+  //     return supertest(app)
+  //       .post("/api/users")
+  //       .send(users[0])
+  //       .then((res) => {
+  //         return supertest(app)
+  //           .post("/api/auth/login")
+  //           .send(users[0])
+  //           .then((res2) => {
+  //             authToken = res2.body.authToken;
+  //           });
+  //       });
+  //   });
+  describe(`POST /api/emails`, () => {});
 });
